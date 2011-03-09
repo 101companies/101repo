5a74999acf593ce24820f895ba26347e8ac124de
rlaemmel <rlaemmel@gmail.com>
2011-03-09 11:02:10 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@408 80f8bc9e-4d75-439a-9954-e8dd026db917

--- haskell/syb/Cut.hs
+++
@@ -1,7 +0,0 @@
-module Cut where
-
-import Company
-import Data.Generics
-
-cut :: Company -> Company
-cut = everywhere (extT id (/(2::Float)))
--- haskell/syb/Total.hs
+++
@@ -1,7 +0,0 @@
-module Total where
-
-import Company
-import Data.Generics
-
-total :: Company -> Float
-total = everything (+) (extQ (const 0) id)
