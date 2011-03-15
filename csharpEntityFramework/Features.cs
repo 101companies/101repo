cc75329dd171ad221d39bf60d4dd143bdb8e7b9b
avaranovich <dotnetby@gmail.com>
2011-03-15 19:59:54 +0000

    added C# Entity Framework implementation
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@475 80f8bc9e-4d75-439a-9954-e8dd026db917

---
+++ csharpEntityFramework/Features.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace csharpEntityFramework
+{
+    public static class Features
+    {
+        public static decimal Total(this CompanyDataContext company)
+        {
+            var res = company.Employees.Sum(emp => emp.Salary);
+            if (res != null) return (decimal) res;
+
+            return 0M;
+        }
+
+        public static void Cut(this CompanyDataContext company)
+        {
+            foreach (var employee in company.Employees)
+            {
+                employee.Salary /= 2;
+            }
+            company.SaveChanges();
+        }
+    }
+}
---
+++ csharpEntityFramework/Tests/FeaturesTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace csharpEntityFramework.Tests
+{
+    [TestFixture]
+    public class FeaturesTests
+    {
+        [Test]
+        public void TotalTest()
+        {
+            /*NOTE: please recreate database before running this test as soon as "Cut" operation modifies the data and total reduced
+             every time you run the test, so we can't expect the same total */
+
+            var company = new CompanyDataContext();
+            var total = company.Total();
+
+            // http://sourceforge.net/apps/mediawiki/developers/index.php?title=101companies is used for a salary source
+            Assert.AreEqual(123456 + 12345 + 1234 + 234567 + 23456 + 2345 + 2344, total);
+
+            company.Cut();
+            var afterCut = company.Total();
+            Assert.AreEqual(total / 2, afterCut);
+        }
+    }
+}
