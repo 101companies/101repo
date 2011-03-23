8bde44243bb6580696b3622c2a15c2a2a62d9ed0
rlaemmel <rlaemmel@gmail.com>
2011-03-23 17:09:11 +0000

    major revision
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@575 80f8bc9e-4d75-439a-9954-e8dd026db917

---
+++ jaxb/org/softlang/jaxb/Cut.java
@@ -0,0 +1,26 @@
+// A variation that involves subtyping
+
+package org.softlang.jaxb;
+
+import org.softlang.company.*;
+
+public class Cut {
+	
+	public static void cut(Company c) {
+		for (Department d : c.getDepartment())
+			cut(d);
+	}
+	
+	public static void cut(Department d) {
+		cut(d.getManager());
+		for (Department s : d.getDepartment())
+			cut(s);
+		for (Employee e : d.getEmployee())
+			cut(e);
+	}
+	
+	public static void cut(Employee e) {
+		e.setSalary(e.getSalary() / 2);
+	}
+		
+}
