1e63d98424c65d9061827fe0bb54a7ca6ac56294
rbonifacio <rbonifacio123@googlemail.com>
2011-03-31 13:41:08 +0000

    Some sample data is being inserted. Should work on the full example next
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@630 80f8bc9e-4d75-439a-9954-e8dd026db917

--- seam/resources/import-dev.sql
+++ seam/resources/import-dev.sql
@@ -1,3 +1,21 @@
 -- SQL statements which are executed at application startup if hibernate.hbm2ddl.auto is 'create' or 'create-drop'
 
 INSERT INTO COMPANY(NAME) VALUES ('Meganalysis');
+
+INSERT INTO SUBUNIT(ID, NAME) VALUES(1, 'Research');
+
+-- Unities representing sub-departments or employees must inform 
+-- the id of the parent department
+
+INSERT INTO DEPARTMENT(ID) VALUES(1);
+  
+INSERT INTO SUBUNIT(ID, NAME, DEPARTMENT_ID) VALUES(2, 'Craig', 1);
+
+INSERT INTO EMPLOYEE(ID, ADDRESS, SALARY) VALUES(2, 'Redmond', 123456);
+
+--
+-- set the department Id. necessary to insert a department 
+-- without a manager because there exists a cyclical dependency.
+--  
+UPDATE DEPARTMENT SET MANAGER_ID=2 WHERE ID = 1 
+ 
