270d9b0a43c184feecb5e996d4422028364fe19d
tsdh <horn@uni-koblenz.de>
2011-04-08 06:45:06 +0000

    implement cut and start with visualization
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@684 80f8bc9e-4d75-439a-9954-e8dd026db917

--- jgralab/src/de/uni_koblenz/oneoonecompanies/CompanyServices.java
+++ jgralab/src/de/uni_koblenz/oneoonecompanies/CompanyServices.java
@@ -1,8 +1,18 @@
 package de.uni_koblenz.oneoonecompanies;
 
+import java.awt.Graphics;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
 import de.uni_koblenz.jgralab.greql2.evaluator.GreqlEvaluator;
 import de.uni_koblenz.jgralab.greql2.jvalue.JValue;
+import de.uni_koblenz.jgralab.utilities.tg2dot.Tg2Dot;
+import de.uni_koblenz.jgralab.utilities.tg2dot.dot.GraphVizOutputFormat;
 import de.uni_koblenz.oneoonecompanies.schema.CompaniesGraph;
+import de.uni_koblenz.oneoonecompanies.schema.Person;
 
 public class CompanyServices {
 	private CompaniesGraph graph;
@@ -21,9 +31,40 @@ public class CompanyServices {
 		return instance;
 	}
 
-	public long totalSumOfSalaries() {
+	public long getSumOfSalaries() {
 		return Math.round(greqlEval(
 				"sum(from p: V{Person} reportSet p.salary end)").toDouble());
+	}
+
+	public void cutSalaries(float factor) {
+		for (Person p : graph.getPersonVertices()) {
+			p.set_salary(Math.round(p.get_salary() / factor));
+		}
+	}
+
+	public void visualizeCompanies() {
+		try {
+			final File png = new File("companies.png");
+			Tg2Dot.convertGraph(graph, png.getCanonicalPath(),
+					GraphVizOutputFormat.PNG);
+			// TODO: add ScrollPanes
+			JFrame f = new JFrame() {
+				private static final long serialVersionUID = -891941166683362373L;
+
+				@Override
+				public void paint(Graphics g) {
+					try {
+						g.drawImage(ImageIO.read(png), 0, 0, null);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			f.setSize(400, 400);
+			f.setVisible(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private JValue greqlEval(String query) {
@@ -33,6 +74,11 @@ public class CompanyServices {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(CompanyServices.instance().totalSumOfSalaries());
+		System.out.println("Before cut: "
+				+ CompanyServices.instance().getSumOfSalaries());
+		CompanyServices.instance().cutSalaries(2);
+		System.out.println("After cut: "
+				+ CompanyServices.instance().getSumOfSalaries());
+		CompanyServices.instance().visualizeCompanies();
 	}
 }
