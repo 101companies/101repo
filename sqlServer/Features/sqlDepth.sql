afd8e4b74f307db9f4958025a9caa144dc7bae8c
avaranovich <dotnetby@gmail.com>
2011-04-15 20:55:14 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@827 80f8bc9e-4d75-439a-9954-e8dd026db917

---
+++ sqlServer/Features/sqlDepth.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROC dbo.ShowHierarchy
+(
+	@Root uniqueidentifier
+)
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @DeptID uniqueidentifier, @DeptName varchar(30)
+
+	SET @DeptName = (SELECT Name FROM dbo.Department WHERE department_id = @Root)
+	PRINT REPLICATE('-', @@NESTLEVEL * 4) + @DeptName
+	PRINT 'Depth:' + Cast(@@NESTLEVEL as nvarchar(40)) 
+
+	SET @DeptID = (SELECT (department_id) FROM dbo.Department WHERE parent_id = @Root)
+
+	WHILE @DeptID IS NOT NULL
+	BEGIN
+		EXEC dbo.ShowHierarchy @DeptID
+		SET @DeptID = (SELECT (department_id) FROM dbo.Department WHERE parent_id = @Root AND department_id > @DeptID)
+	END
+END
+GO
