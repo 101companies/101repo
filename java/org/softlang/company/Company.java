855aef148f051a00f41b3fa244460f6ca873684b
rlaemmel <rlaemmel@gmail.com>
2011-04-18 03:20:47 +0000

    cosmetics
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@851 80f8bc9e-4d75-439a-9954-e8dd026db917

--- java/org/softlang/company/Company.java
+++ java/org/softlang/company/Company.java
@@ -1,10 +1,5 @@
 package org.softlang.company;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
