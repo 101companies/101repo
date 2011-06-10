72af54c57ffcb845700a97b1307a4093918f7732
rlaemmel <rlaemmel@gmail.com>
2011-06-10 19:00:56 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@1299 80f8bc9e-4d75-439a-9954-e8dd026db917

--- javaSyb/org/softlang/features/Serialization.java
+++ javaSyb/org/softlang/features/Serialization.java
@@ -5,8 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.Field;
-import java.util.LinkedList;
 
 import org.softlang.company.Company;
 
