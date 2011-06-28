9a2293fbd1dcf969f84ccf01ca46a986d51ec219
tzimmer <tzimmer@uni-koblenz.de>
2011-06-28 17:26:07 +0000

    html5 session storage some new styles
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@1442 80f8bc9e-4d75-439a-9954-e8dd026db917

--- html5session/employeeView.js
+++ html5session/employeeView.js
@@ -14,18 +14,20 @@ gui.generateFormular = function() {
 	
 	// employee name
 	content += "<tr><td align=\"right\">Name: </td>";
-	content += "<td><input type=\"text\" name=\"name\" value='" + model.name + "'>";
+	content += "<td><input type=\"text\" name=\"name\" class=\"text\" value='" + model.name + "'></td></tr>";
 	
 	// employee address
 	content += "<tr><td align=\"right\">Address: </td>";
-	content += "<td><input type=\"text\" name=\"address\" value='" + model.address + "'>";
+	content += "<td><input type=\"text\" name=\"address\" class=\"text\" value='" + model.address + "'></td></tr>";
 	
 	// employee salary
 	content += "<tr><td align=\"right\">Salary: </td>";
-	content += "<td><input type=\"text\" name=\"salary\" value='" + model.salary + "'>";	
+	content += "<td><input type=\"text\" name=\"salary\" class=\"text\" value='" + model.salary + "'></td></tr>";	
+	
+	
+	content += "<tr><td colspan=\"2\"><input type=\"button\" name=\"save\" class=\"button\" value=\"save\" onclick=\"model.save(" + employeeId + ", this.form.name.value, this.form.address.value, this.form.salary.value)\"></td></tr>";
 	content += "</table>";
 	
-	content += " <input type=\"button\" name=\"save\" value=\"save\" onclick=\"model.save(" + employeeId + ", this.form.name.value, this.form.address.value, this.form.salary.value)\"></td></tr>";
 	content += "</form>";
 
 	// move content to form
