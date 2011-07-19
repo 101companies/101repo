41c166a3924aac17f25da7d6038224a6d9deb621
avaranovich <dotnetby@gmail.com>
2011-07-19 11:34:43 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@1505 80f8bc9e-4d75-439a-9954-e8dd026db917

---
+++ wpf/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TODO: bind hierarchical data with infinite level of nesting
+            // http://social.msdn.microsoft.com/forums/en-US/wpf/thread/3a319a26-a910-47ef-88eb-97af09e29082/
+            // http://msdn.microsoft.com/en-us/library/system.windows.hierarchicaldatatemplate(vs.85).aspx
+        }
+    }
+}
