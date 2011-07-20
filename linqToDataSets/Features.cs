31c839f8201abee93363d7e61fdd52359b28bc84
avaranovich <dotnetby@gmail.com>
2011-07-20 11:58:33 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@1507 80f8bc9e-4d75-439a-9954-e8dd026db917

---
+++ linqToDataSets/Features.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linqToDataSets
+{
+    public static class Features
+    {
+        public static decimal Total()
+        {
+            var ds = new CompanyDataSet();
+            var ed = new CompanyDataSetTableAdapters.EmployeeTableAdapter();
+            ed.Fill(ds.Employee);
+           
+            var total = ds.Employee.Sum(e => e.Salary);
+            return total;
+        }
+
+        public static void Cut()
+        {
+            var ds = new CompanyDataSet();
+            var ed = new CompanyDataSetTableAdapters.EmployeeTableAdapter();
+            ed.Fill(ds.Employee);
+
+            foreach (var emp in ds.Employee)
+            {
+                emp.Salary /= 2;
+            }
+
+            ed.Update(ds);
+        }
+    }
+}
---
+++ linqToDataSets/Tests/FeaturesTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace linqToDataSets.Tests
+{
+    [TestFixture]
+    public class FeaturesTests
+    {
+        [Test]
+        public void TotalTest()
+        {
+            var total = Features.Total();
+            Assert.AreEqual(399747M, total);
+
+        }
+
+        [Test]
+        public void CutTest()
+        {
+            var totalBefore = Features.Total();
+            Features.Cut();
+            var totalAfter = Features.Total();
+
+            Assert.AreEqual(totalBefore / 2, totalAfter);
+
+        }
+    }
+}
