e35d210ad560404343f343a5d94e0d39bbe3aaab
tzimmer <tzimmer@uni-koblenz.de>
2011-09-06 12:27:08 +0000

    html5tree
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@1813 80f8bc9e-4d75-439a-9954-e8dd026db917

--- html5tree/client/javascript/view/treeView.js
+++ html5tree/client/javascript/view/treeView.js
@@ -7,7 +7,7 @@ treeView.refresh = function() {
     if (model.response.departments.length > 0) {
         content += "<li> <input id=\"0\" type=\"image\" src=\"symbols/plus.gif\" onclick=\"treeNavigation.toggleList(this)\">";
         content += "<input type=\"button\" class=\"companyButton\" value=\"" + model.response.name + "\" onclick=\"controller.loadCompany(" + model.response.id + ")\">";
-        content += treeView.showDepartments(model.response.departments, "0");
+        content += treeView.showDepartments(model.response.departments);
         content += "</li>";
     } else {
         content += "<li> <img src=\"symbols/leaf.gif\"> <b>" + model.response.name + "</b></li>";
@@ -18,19 +18,18 @@ treeView.refresh = function() {
     document.querySelector('#tree').innerHTML = content;
 }
 
-treeView.showDepartments = function(deps, id) {
-    content = "<ul id=\"" + id + "\" style=\"display:none\">";
+treeView.showDepartments = function(deps) {
+    content = "<ul style=\"display:none\">";
     
     for (var i = 0; i < deps.length; i++) {
         if (deps[i].employees.length > 0 || deps[i].departments.length > 0) {
-            var idTemp = id + i;
-            content += "<li> <input id=\"" + idTemp + "\" type=\"image\" src=\"symbols/plus.gif\" onclick=\"treeNavigation.toggleList(this)\">";
+            content += "<li> <input type=\"image\" src=\"symbols/plus.gif\" onclick=\"treeNavigation.toggleList(this)\">";
             content += "<input type=\"button\" class=\"departmentButton\" value=\"" + deps[i].name + "\" onclick=\"controller.loadDepartment(" + deps[i].id + ")\">";
             if (deps[i].employees.length > 0) {
-                content += treeView.showEmployees(deps[i].employees, idTemp);
+                content += treeView.showEmployees(deps[i].employees);
             }
             if (deps[i].departments.length > 0) {
-                content += treeView.showDepartments(deps[i].departments, idTemp);
+                content += treeView.showDepartments(deps[i].departments);
             }
             content += "</li>";
         } else {
@@ -44,8 +43,8 @@ treeView.showDepartments = function(deps, id) {
     return content;
 }
 
-treeView.showEmployees = function(employees, id) {
-    content = "<ul id=\"" + id + "\" style=\"display:none\">";
+treeView.showEmployees = function(employees) {
+    content = "<ul style=\"display:none\">";
     
     for (var i = 0; i < employees.length; i++) {
         if (employees[i].manager == false) {
--- html5tree/jquery/treeNavigation.js
+++ html5tree/jquery/treeNavigation.js
@@ -1,21 +1,18 @@
 var treeNavigation = {
     toggleList: function(el) {
         var minimized = jQuery(el).prop("minimized");
-        var id = jQuery(el).prop("id");
         
         if (minimized || minimized == null) {
             jQuery(el).prop("src", "symbols/minus.gif");
-            jQuery(el).parent().find("ul").each(function() {
-                if (jQuery(this).prop("id") == id ) {
-                    jQuery(this).css("display", "block");
-                }
+            jQuery(el).parent().children("ul").each(function() {
+                jQuery(this).css("display", "block");
+                
             });
         } else {
             jQuery(el).prop("src", "symbols/plus.gif");
-            jQuery(el).parent().find("ul").each(function() {
-                if (jQuery(this).prop("id") == id ) {
-                    jQuery(this).css("display", "none");
-                }
+            jQuery(el).parent().children("ul").each(function() {
+                jQuery(this).css("display", "none");
+                
             });
         }
         if (minimized == null) {
