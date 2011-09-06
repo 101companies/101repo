5dd669803c7e121013b89bacbf3f3ea89540b839
tschmorleiz <tschmorleiz@uni-koblenz.de>
2011-09-06 12:43:44 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@1816 80f8bc9e-4d75-439a-9954-e8dd026db917

--- hxtPickler/Main.hs
+++ hxtPickler/Main.hs
@@ -8,24 +8,10 @@ import Pickler
 import Total
 import Cut
 
-getCompany :: [Company] -> IO Company
-getCompany cs = do
-    
-    -- exit on deserialization failure
-    if (null cs || length cs /= 1) 
-      then error "Company could not be unpickled."
-      else return () 
-    
-    -- extract company from arrow result
-    return $ head cs
-
 
 main = do
     -- deserializing from XML
-    cs <- runX ( xunpickleDocument xpCompany [withRemoveWS yes] "sampleCompany.xml" )
-    
-    -- extract company from arrow results
-    company1 <- getCompany cs 
+    [company1] <- runX ( xunpickleDocument xpCompany [withRemoveWS yes] "sampleCompany.xml" )
     
     -- cut
     let cutCompany = cut company1
@@ -34,15 +20,13 @@ main = do
     print $ total company1   
     
     -- serilize and deserialize
-    cs2 <- runX ( constA cutCompany -- lifts company 
+    [company2] <- runX ( constA cutCompany -- lifts company 
                   >>>
 	                xpickleDocument xpCompany [withIndent yes] "sampleCompanyCut.xml"
 	                >>>
 	                xunpickleDocument xpCompany [withRemoveWS yes] "sampleCompanyCut.xml"  
                   )
-         
-    company2 <- getCompany cs2 
-    
+          
     -- print total
     print $ total company2    
     
