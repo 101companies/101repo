275d9e016a218a025fb3036310ccf8c826c83afd
avaranovich <dotnetby@gmail.com>
2011-09-13 13:38:00 +0000

    initial import
    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@1882 80f8bc9e-4d75-439a-9954-e8dd026db917

---
+++ xsdClasses/Features.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public partial class company
+{
+    public void Cut()
+    {
+        foreach (var dept in department)
+        {
+            //dept.Cut();
+        }
+    }
+
+    public double Total
+    {
+        get
+        {
+             return department.Sum(dept => dept.Total);
+        }
+    }
+}
+
+public partial class department
+{
+    /// <summary>
+    /// Total all salaries in a department and subdepartments. 
+    /// </summary>
+    public double Total
+    {
+        get
+        {
+            // calculate total across sub units
+
+            //add current division's employees' salaries to the total
+
+            return manager.salary + department1.Sum(department => department.Total) +
+                employee.Sum(e => e.salary);
+        }
+    }
+
+    /// <summary>
+    /// Cut all salaries in half in department and subdepartments. 
+    /// </summary>
+    public void Cut()
+    {
+        foreach (var department in department1)
+        {
+            department.Cut();
+        }
+
+        manager.salary /= 2;
+
+        foreach (var emp in employee.Where(e => e.salary != 0.0))
+        {
+            emp.salary /= 2;
+        }
+    }
+}
