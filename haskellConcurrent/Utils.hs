a00a4c9c919afd6889a9a185bcfc594fbb80b05a
tschmorleiz <tschmorleiz@uni-koblenz.de>
2011-09-24 10:30:28 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@2006 80f8bc9e-4d75-439a-9954-e8dd026db917

--- haskellConcurrent/Utils.hs
+++ haskellConcurrent/Utils.hs
@@ -2,21 +2,5 @@ module Utils where
 
 import Control.Concurrent
 
-rotate :: [a] -> [a]
-rotate (x:xs) = xs ++ [x]
-
-derotate :: [a] -> [a]
-derotate xs = last xs : init xs
-
 takeAllMVars ::  [MVar a] -> IO [a]
-takeAllMVars [] = return []
-takeAllMVars (m:ms) = do
-    x <- tryTakeMVar m
-    f x
-    where
-        f (Just y) = do
-            ys <- takeAllMVars ms
-            return $ y:ys
-        f (Nothing) = do
-            ys <- takeAllMVars $ rotate $ m:ms
-            return $ derotate ys
+takeAllMVars = mapM takeMVar
