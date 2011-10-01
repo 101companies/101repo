3134e7d20e1ac0223fb7c7f7a166c19a2d30c058
tschmorleiz <tschmorleiz@uni-koblenz.de>
2011-10-01 21:06:44 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@2090 80f8bc9e-4d75-439a-9954-e8dd026db917

--- hdbc/Total.hs
+++ hdbc/Total.hs
@@ -2,14 +2,9 @@ module Total where
 
 import Database.HDBC 
 
-total :: IConnection conn => conn -> String -> IO (Maybe Double)
+total :: IConnection conn => conn -> String -> IO Double
 total conn cName = do
-     stmt <- prepare conn "SELECT sum(salary) FROM employee, company WHERE company.name = ? and company.id = employee.cid"
+     stmt <- prepare conn "SELECT salary FROM employee, company WHERE company.name = ? and company.id = employee.cid"
      execute stmt [toSql cName]
-     res <- fetchAllRows' stmt
-     return (toMaybe $ head $ head res)
-        where
-            toMaybe total@(SqlDouble _) = Just (fromSql total :: Double)
-            toMaybe SqlNull = Nothing
-    
-                  
+     res <- fetchAllRows stmt
+     return $ sum (map (fromSql.head) res)            
