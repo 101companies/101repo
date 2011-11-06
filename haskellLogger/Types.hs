8cd4c471149c962aefa241d51120974f4a50c230
tschmorleiz <tschmorleiz@uni-koblenz.de>
2011-11-06 21:07:05 +0000

    git-svn-id: https://developers.svn.sourceforge.net/svnroot/developers/repository/101companies@2315 80f8bc9e-4d75-439a-9954-e8dd026db917

--- haskellLogger/Types.hs
+++ haskellLogger/Types.hs
@@ -4,8 +4,4 @@ module Types where
 type Log = [String] 
 
 ppLog :: Log -> String
-ppLog = concat.(map ((++) "\n"))
-
-composeLog :: String -> String -> String -> String -> Int
-composeLog p a c n o t =
-  a ++ " " ++ a ++ " " ++ c ++ " \"" ++ n ++ "\" , " ++ o ++ " total = " ++ t      
+ppLog = concat.(map ((++) "\n")) 
