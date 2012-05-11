9c7ad9f709535d24ce1994a80fc1d126b9c20631
Ralf Laemmel <rlaemmel@gmail.com>
2012-05-10 23:19:14 -0500

    Working on XmlFragmentLocator

---
+++ technologies/XmlFragmentSelector/locator.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+import commands
+import os.path
+import simplejson
+import sys
+
+if len(sys.argv) != 4:
+    sys.exit("Usage: locator.py inputFile fragmentFile linesFile")
+locatorPy = sys.argv[0]
+x = os.path.dirname(locatorPy)
+command = "cd " + x + "; make"
+status, output = commands.getstatusoutput(command)
+if (status):
+    print output
+    sys.exit(status)
+command = "java -classpath " + x + " locator/XmlFragmentLocator " + sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3]
+print command
+status, output = commands.getstatusoutput(command)
+if (status):
+    print output
+    sys.exit(status)
--- technologies/XmlFragmentSelector/locator/XmlFragmentSelector.java
+++ technologies/XmlFragmentSelector/locator/XmlFragmentSelector.java
@@ -19,7 +19,7 @@ import org.xml.sax.SAXException;
  *
  * @author Martin
  */
-public class XMLFragmentSelector {
+public class XmlFragmentSelector {
     
     /**
      * @param args the command line arguments
