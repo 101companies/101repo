886ee6d2e1a66a9602de5abb4a64a5b75f8e6ac3
Ralf Laemmel <rlaemmel@gmail.com>
2012-06-02 23:34:04 +0200

    Simplified writerMonad

--- contributions/writerMonad/Main.hs
+++ contributions/writerMonad/Main.hs
@@ -6,20 +6,9 @@ import Control.Monad.Writer
 import Company
 import SampleCompany
 import Types
-import Total
 import Cut
 
 main = do
-    -- totaling
-    let (ttl,log) = runWriter $ totalLogCompany company
-    print $ ttl
-    putStrLn $ ppLog log
-    
     -- cutting
     let (cutC, log) = runWriter $ cutLogCompany company
-    putStrLn $ ppLog log
-    
-    -- totaling
-    let (ttl, log) = runWriter $ totalLogCompany cutC
-    print $ ttl
     putStrLn $ ppLog log
