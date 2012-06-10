b0ca03eb293524b6d0e8d6cdc96ca527af792bed
Ralf Laemmel <rlaemmel@gmail.com>
2012-06-10 11:31:31 -0400

    Work implied by work on 101meta

--- contributions/haskell/Company.hs
+++ contributions/haskell/Company.hs
@@ -4,11 +4,11 @@ data Company = Company Name [Department]
  deriving (Eq, Read, Show)
 data Department = Department Name Manager [SubUnit]
  deriving (Eq, Read, Show)
-type Manager = Employee
 data Employee = Employee Name Address Salary
  deriving (Eq, Read, Show)
 data SubUnit = EUnit Employee | DUnit Department
  deriving (Eq, Read, Show)
+type Manager = Employee
 type Name = String
 type Address = String
 type Salary = Float
