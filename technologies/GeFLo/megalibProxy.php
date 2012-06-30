e95567015ad27ee2526d6f5032a8dd5e6d8bc8e6
megaplanet <megaplanet.github@megaplanet.org>
2012-06-30 12:59:03 +0200

    add support for searching configuration files

--- technologies/GeFLo/megalibProxy.php
+++ technologies/GeFLo/megalibProxy.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once dirname(__FILE__).'/../../../101worker/configs/main.config.local.php';
-
-if (DEBUG > 5) echo "Using megalib from ".ABSPATH_MEGALIB."\n";
-
-if (!defined('ABSPATH_SRC_GESHI_LIBRARY')) {
-	define('ABSPATH_SRC_GESHI_LIBRARY', ABSPATH_BASE.'101repo/technologies/geshi/src');
+// Search for the condiguration file 
+$configFile = '../../../101worker/configs/main.config.local.php' ;
+if (! is_file($configFile)) {
+  $configFile = "../".$configFile ;
 }
+require_once $configFile;
 
-if (DEBUG > 5) echo "Using GeSHi from ".ABSPATH_SRC_GESHI_LIBRARY."\n";
+if (DEBUG > 10) echo "Using megalib from ".ABSPATH_MEGALIB."\n";
+if (DEBUG > 10) echo "Using GeSHi from ".ABSPATH_SRC_GESHI_LIBRARY."\n";
 
 require_once ABSPATH_MEGALIB.'SourceFiles.php';
 
@@ -24,20 +24,6 @@ if (!is_file($path)) {
 	echo "ERROR: The first argument has to be a file!";
 	exit(1);
 }
-
-// $matcher	= new GeSHiExtensionPatternMatcher();
-// $fileInfo	= $matcher->matchPath('file', $path);
-// if (empty($fileInfo)) {
-//	echo "ERROR: Unknown extension for GeSHi! Can\'t parse file contents!";
-//	exit(1);
-//}
-
-// $contents	= file_get_contents($path);
-// $sourceCode	= new SourceCode($contents, $fileInfo['geshiLanguage']);
-
-// Box the token array in an associative array
-// echo json_encode(array('tokens' => $sourceCode->getTokens()));
-
 $sourceFile = new SourceFile($path) ;
 echo json_encode($sourceFile->getSourceCode()->getTokens()) ;
 exit(0);
