dbca0359bc33afc3a28efd8e0d8dc29c04df21f5
Martin Leinberger <martin.leinberger@googlemail.com>
2012-07-11 00:44:43 +0200

    Modified import and annotation matching scripts They can now take multiple imports / attributes as arguments

--- technologies/Java_platform/javaImport.py
+++ technologies/Java_platform/javaImport.py
@@ -3,8 +3,8 @@
 import sys
 import json
 
-if len(sys.argv) != 3:
-    sys.exit("Usage: factImport.py sourceFile import")
+if len(sys.argv) < 3:
+    sys.exit("Usage: factImport.py sourceFile import [import ...]")
 
 extractPath = sys.argv[1] + '.extractor.json';
 imported = sys.argv[2];
@@ -13,7 +13,8 @@ factsFile = open(extractPath);
 facts = json.load(factsFile);
 
 for x in facts["imports"]:
-	if x == imported:
-		sys.exit(0);
+	for i in range(2, len(sys.argv)):
+		if x == sys.argv[i]:
+			sys.exit();
 
 sys.exit(1);
