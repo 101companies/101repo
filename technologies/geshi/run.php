8b364876897d58e23b8f7e389ec09bd856a3d363
Ralf Laemmel <rlaemmel@gmail.com>
2012-07-20 19:11:22 +0200

    Generated line numbers for GeSHi

--- technologies/geshi/run.php
+++ technologies/geshi/run.php
@@ -9,9 +9,9 @@ $language = $argv[3];
 $text = file_get_contents($ifilename) or die("can't open input file");
 
 $geshi = new GeSHi();
-$geshi->set_source($text) ;
-$geshi->set_language($this->geshiLanguage) ; 
-$geshi->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS) ;
+$geshi->set_source($text);
+$geshi->set_language($language); 
+$geshi->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS);
 $geshi->enable_classes();
 $geshi->enable_ids(true);
 
