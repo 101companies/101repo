48613bf2229d276e1ffff1f2a46a6d511c508ab7
Ralf Laemmel <rlaemmel@gmail.com>
2012-07-20 19:00:22 +0200

    GeSHi issues

--- technologies/geshi/run.php
+++ technologies/geshi/run.php
@@ -3,21 +3,21 @@
 include_once 'src/geshi.php';
 
 $ifilename = $argv[1];
-
 $ofilename = $argv[2];
-
 $language = $argv[3];
 
-$input = file_get_contents($ifilename) or die("can't open input file");
+$text = file_get_contents($ifilename) or die("can't open input file");
 
-$geshi = new GeSHi($input, $language);
+$geshi = new GeSHi();
+$geshi->set_source($text) ;
+$geshi->set_language($this->geshiLanguage) ; 
+$geshi->enable_line_numbers(GESHI_NORMAL_LINE_NUMBERS) ;
+$geshi->enable_classes();
+$geshi->enable_ids(true);
 
 $output = $geshi->parse_code();
-
 $ofile = fopen($ofilename, 'w') or die("can't open output file");
-
 fwrite($ofile, $output);
-
 fclose($ofile);
 
 ?>
