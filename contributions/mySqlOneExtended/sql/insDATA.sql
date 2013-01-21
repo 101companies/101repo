423e410da08262447c94ea408ba37bea6162860a
Yves Dupont <ydupont@yd-nb1.(none)>
2013-01-21 11:27:18 +0100

    Initial commit for Contribution:mySqlOneExtended

---
+++ contributions/mySqlOneExtended/sql/insDATA.sql
@@ -0,0 +1,8 @@
+-- SQL File
+-- Yves Dupont <ydupont@uni-koblenz.de>
+--
+-- Host: yd.wks001.int.ngineer.de
+-- Generation Time: Jan 19, 2013 at 10:51 AM
+-- MySQL server version: 5.5.28
+
+-- TO BE COMPLETED
