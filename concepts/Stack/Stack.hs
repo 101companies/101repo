4752d6816e2911a80ae922ffb28e0548facdf84b
Ralf Laemmel <rlaemmel@gmail.com>
2013-03-02 23:56:14 +0100

    Started the idea of having source code for concepts in 101repo

---
+++ concepts/Stack/Stack.hs
@@ -0,0 +1,29 @@
+-- A simple implementation of stacks in Haskell
+
+module Stack where
+
+-- Data structure for representation of stacks
+data Stack = Empty | Push Int Stack
+ 
+{- Operations of stacks -}
+ 
+-- Return the empty stack
+empty = Empty
+ 
+-- Test for the empty stack
+isEmpty :: Stack -> Bool
+isEmpty Empty = True
+isEmpty (Push _ _) = False
+ 
+-- Push an element onto the stack
+push :: Int -> Stack -> Stack
+push x s = Push x s
+ 
+-- Retrieve the top-of-stack, if available
+top :: Stack -> Int
+top (Push x s) = x
+ 
+-- Remove the top-of-stack, if available
+pop :: Stack -> Stack
+pop (Push x s) = s
+
