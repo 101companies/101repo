4fbc8d455a8c819f75ad9ff046a2d24fecf94532
martinleinberger <martin.leinberger@googlemail.com>
2013-03-06 16:16:58 +0100

    updated technologies to use javaImport.py script (relates to issue #42)
    - changed all occurrences of javaImport.sh to javaImport.py
    - fixed javaImport.py, since it was written in a time where const101.sRoot == const101.tRoot
    - also, I pretty printed all technologies, so that intendation and stuff like that should be unified in all of them

--- technologies/Java_platform/javaImport.py
+++ technologies/Java_platform/javaImport.py
@@ -2,19 +2,24 @@
 
 import sys
 import json
+import os
+
+sys.path.append('../../libraries/101meta')
+import const101
+
 
 if len(sys.argv) < 3:
     sys.exit("Usage: javaImport.py sourceFile import [import ...]")
 
-extractPath = sys.argv[1] + '.extractor.json';
-imported = sys.argv[2];
+extractPath = os.path.join(const101.tRoot, sys.argv[1] + '.extractor.json')
+imported = sys.argv[2]
 
-factsFile = open(extractPath);
-facts = json.load(factsFile);
+factsFile = open(extractPath)
+facts = json.load(factsFile)
 
 for x in facts["imports"]:
-	for i in range(2, len(sys.argv)):
-		if x == sys.argv[i]:
-			sys.exit();
+    for i in range(2, len(sys.argv)):
+        if x == sys.argv[i]:
+            sys.exit()
 
-sys.exit(1);
+sys.exit(1)
