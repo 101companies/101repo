ec68959d114eafcb0b165c2d797e403187640e3f
martinleinberger <martin.leinberger@googlemail.com>
2013-04-18 22:35:51 +0200

    bugfixing in javaAnnotation and javaImport predicates

--- technologies/Java_platform/javaAnnotation.py
+++ technologies/Java_platform/javaAnnotation.py
@@ -2,19 +2,42 @@
 
 import sys
 import json
+import os
+
+sys.path.append('../../libraries/101meta')
+import const101
+
+
+def hasAnnotation(annotation, fragments):
+    for fragment in fragments:
+        #search in current fragment
+        for anno in fragment.get('annotations', []):
+            if anno == annotation:
+                return True
+
+        #search in sub fragments
+        if hasAnnotation(annotation, fragment.get('fragments', [])):
+            return True
+
+    return False
+
 
 if len(sys.argv) < 3:
-    sys.exit("Usage: factAnnotation.py sourceFile attribute [attribute ...]")
+    sys.exit("Usage: javaAnnotation.py attribute [attribute ...] sourceFile")
 
-extractPath = sys.argv[1] + '.extractor.json';
+path = sys.argv[len(sys.argv) - 1]
+if const101.sRoot in path:
+    path = path[len(const101.sRoot) + 1:]
 
-factsFile = open(extractPath);
-facts = json.load(factsFile);
+extractPath = os.path.join(const101.tRoot, path + '.extractor.json')
 
-for decl in facts["declarations"]:
-	for attr in decl["attributes"]:
-		for i in range(2, len(sys.argv)):
-			if attr == sys.argv[i]:
-				exit()
+factsFile = open(extractPath)
+facts = json.load(factsFile)
 
-sys.exit(1);
+for i in range(1, len(sys.argv) - 1):
+    if hasAnnotation(sys.argv[i], facts['fragments']):
+        #exit with status code 0 indicating success - import has been found
+        sys.exit()
+
+#exit with status code 1, indicating failure - import has not been found
+sys.exit(1)
--- technologies/Java_platform/javaImport.py
+++ technologies/Java_platform/javaImport.py
@@ -9,17 +9,22 @@ import const101
 
 
 if len(sys.argv) < 3:
-    sys.exit("Usage: javaImport.py sourceFile import [import ...]")
+    sys.exit("Usage: javaImport.py import [import ...] sourceFile")
 
-extractPath = os.path.join(const101.tRoot, sys.argv[1] + '.extractor.json')
-imported = sys.argv[2]
+path = sys.argv[len(sys.argv) - 1]
+if const101.sRoot in path:
+    path = path[len(const101.sRoot) + 1:]
+
+extractPath = os.path.join(const101.tRoot, path + '.extractor.json')
 
 factsFile = open(extractPath)
 facts = json.load(factsFile)
 
 for x in facts["imports"]:
-    for i in range(2, len(sys.argv)):
+    for i in range(1, len(sys.argv) - 1):
         if x == sys.argv[i]:
+            #exit with status code 0 indicating success - import has been found
             sys.exit()
 
+#exit with status code 1, indicating failure - import has not been found
 sys.exit(1)
