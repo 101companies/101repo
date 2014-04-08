e54e7791e70290787cc97493d605a4cb59b7d067
Ralf Lämmel <rlaemmel@gmail.com>
2014-04-08 13:23:06 +0200

    minor edits

--- concepts/Greatest_common_divisor/GCD.py
+++ concepts/Greatest_common_divisor/GCD.py
@@ -1,17 +1,2 @@
-# Assume operands to be positive
-
-def gcdIterative(x, y):
-    while x != y:
-        if x > y: 
-            x -= y
-        else:
-            y -= x 
-    return x
-
-def gcdRecursive(x, y):
-    if x > y: 
-        return gcdRecursive(x-y, y)
-    elif x > y: 
-        return gcdRecursive(x, y-x)
-    else:
-        return x
+while 1 == 1:
+    x = 42
